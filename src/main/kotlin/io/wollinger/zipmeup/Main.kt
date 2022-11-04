258220134f0ada4462a46f3dbba24534d2316dda
SvenWollinger <accounts@wollinger.email>
2022-11-04 14:28:17 +0100

    Add settings to log

--- src/main/kotlin/io/wollinger/zipmeup/Main.kt
+++ src/main/kotlin/io/wollinger/zipmeup/Main.kt
@@ -3,27 +3,42 @@ package io.wollinger.zipmeup
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.lingala.zip4j.ZipFile
 import java.io.File
+import java.lang.StringBuilder
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
+import java.time.LocalDateTime
 import kotlin.system.exitProcess
+
+const val VERSION = "0.0.1"
+
+class Logger(private val settings: Settings) {
+    var log = ""
+    fun log(msg: String) {
+        if(settings.log) println(msg)
+        if(settings.includeLog) log += "$msg\n"
+    }
+}
 
 fun main(args: Array<String>) {
     if(args.isEmpty()) println("Supply a valid json file in the arguments!").also { exitProcess(0) }
+    when(args[0]) {
+        "-v", "-version" -> {
+            println(VERSION)
+            exitProcess(0)
+        }
+    }
 
     val configFile = File(args[0])
-    val settings = ObjectMapper().readValue(configFile, Settings::class.java)
+    val settings = ObjectMapper().readValue(configFile, Settings::class.java).also { it.format() }
+    val logger = Logger(settings)
+
+    logger.log("Starting ZipMeUp (v$VERSION) on ${LocalDateTime.now()}")
+    logger.log("File: ${configFile.absolutePath}")
+    logger.log("Settings:")
+    logger.log(settings.toString())
 
     val firstOutput = settings.output.removeFirst()
-    var path = Utils.formatEnv(firstOutput)
-    path = if(settings.formatOutput) Utils.formatTime(path) else path
-
-    ZipFile(path).run {
-        var log = ""
-        fun log(msg: String) {
-            if(settings.log) println(msg)
-            if(settings.includeLog) log += "$msg\n"
-        }
-
+    ZipFile(firstOutput).run {
         ArrayList<File>().also { allFiles ->
             //Collect files
             settings.input.forEach { path ->
@@ -32,7 +47,7 @@ fun main(args: Array<String>) {
 
             //Add files to zip file
             allFiles.forEachIndexed { index, file ->
-                log("[${index+1}/${allFiles.size}] $file")
+                logger.log("[${index+1}/${allFiles.size}] $file")
                 addFile(file, Utils.getParams(file))
             }
         }
@@ -42,21 +57,21 @@ fun main(args: Array<String>) {
             val zippedConfig = File("ZipMeUpConfig.json").also { it.deleteOnExit() }
             Files.copy(configFile.toPath(), zippedConfig.toPath())
             addFile(zippedConfig, Utils.getParams(zippedConfig))
-            log("Adding config...")
+            logger.log("Adding config...")
         }
 
         //Add log if requested
         if(settings.includeLog) {
             val logFile = File("ZipMeUpLog.log").also { it.deleteOnExit() }
-            Files.writeString(logFile.toPath(), log)
+            Files.writeString(logFile.toPath(), logger.log)
             addFile(logFile, Utils.getParams(logFile))
-            log("Adding log...")
+            logger.log("Adding log...")
         }
     }
 
     //Create additional copies if requested
     settings.output.forEach {
-        Files.copy(File(path).toPath(), File(it).toPath(), StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(File(firstOutput).toPath(), File(it).toPath(), StandardCopyOption.REPLACE_EXISTING)
     }
 }
 
@@ -67,4 +82,26 @@ class Settings {
     var formatOutput = false
     var input = ArrayList<String>()
     var output = ArrayList<String>()
+
+    fun format() {
+        //Format input
+        ArrayList<String>().also { it.addAll(input) }.forEachIndexed { i, s ->
+            input[i] = Utils.formatEnv(s)
+        }
+        //Format output
+        ArrayList<String>().also { it.addAll(output) }.forEachIndexed{ i, s ->
+            output[i] = Utils.formatEnv(s)
+            if(formatOutput) output[i] = Utils.formatTime(output[i])
+        }
+    }
+
+    override fun toString() = StringBuilder().also { sb ->
+        sb.append("\tincludeConfig: $includeConfig\n")
+        sb.append("\tincludeLog: $includeLog\n")
+        sb.append("\tlog: $log\n")
+        sb.append("\tinput:\n")
+        input.forEach { sb.append("\t+ $it\n") }
+        sb.append("\toutput:\n")
+        output.forEach { sb.append("\t+ $it\n") }
+    }.toString()
 }
